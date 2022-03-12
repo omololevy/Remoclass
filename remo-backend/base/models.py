f674a6a97c40e63db63f6bdac4f3da65ac0afd1e
omololevy <cotechlevy@gmail.com>
2022-03-12 16:17:01 +0300

    Create the first table column

--- remo-backend/base/models.py
+++ remo-backend/base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Unit(models.Model):
+    title =  models.CharField(max_length=50, blank=False, default='')
+    description = models.CharField(max_length=250,blank=False, default='')
+      
